aadde30e645f24de7d071f742834265779ec7d64
rendfall <rendfall@gmail.com>
2017-11-11 02:00:21 +0100

    Add config file

---
+++ src/config.js
@@ -0,0 +1,9 @@
+import Phaser from 'phaser'
+
+export default {
+    type: Phaser.AUTO,
+    parent: 'content',
+    width: 800,
+    height: 600,
+    localStorageName: 'gemstone-mine'
+}
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import 'pixi';
 import 'p2';
 import Phaser from 'phaser';
 
+import config from './config';
+
 class Game extends Phaser.Game {
-    constructor () {
-        super();
+    constructor() {
+        super(config);
     }
 }
 
